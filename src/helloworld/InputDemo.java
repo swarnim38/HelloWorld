1b359f7ac3d213c168dd316303eaa3e280925696
swarnim38 <swarnim.marathe@gmail.com>
2023-09-30 08:36:35 +0530

    inputdemo

--- src/helloworld/InputDemo.java
+++ src/helloworld/InputDemo.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package helloworld;
+import java.util.Scanner;
 
 /**
  *
  * @author swarn
  */
 public class InputDemo {
+    
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        String name = user_input.next();
+        System.out.println("Hello"+name);
+        String age_string = user_input.next();
+        int age = Integer.parseInt(age_string);
+        System.out.println("In 20 years I will be"+ (age + 20));
+        System.out.println(" years old");
+    }
+    
+    
     
 }
